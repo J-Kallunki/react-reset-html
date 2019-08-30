ba36e872b0d39dcdf3af1118465d154351e0565b
Joonas Kallunki <jkallunki@gmail.com>
2019-08-30 19:36:04 +0300

    Create common reset styles

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,22 @@
+import { css } from 'styled-components';
 import {
   normalizeCssInJs,
   resetWithSelectors as libResetWithSelectors,
 } from 'normalize.cssinjs';
 
 const normalize = normalizeCssInJs({ cssToString: true });
+
+const common = css`
+  margin: 0;
+  padding: 0;
+  border: 0;
+  box-sizing: border-box;
+  font: 100% inherit;
+  line-height: 1;
+  text-align: left;
+  text-decoration: none;
+  vertical-align: baseline;
+  color: inherit;
+  background: none;
+  cursor: inherit;
+`;
